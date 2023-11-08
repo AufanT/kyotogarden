9d462e8f271bf576b7078917fc817ec970cc98fd
AufanT <aufantaufiq08905@gmail.com>
2023-11-08 20:30:41 +0700

    membuat login popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const navbarNav = document.querySelector('.navbar-nav');
 const searchMenu = document.querySelector('.search-menu');
 const navbar = document.querySelector('.navbar');
 const searchBox = document.querySelector('#search-box');
+const loginMenu = document.querySelector('.login-menu');
 
 document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
@@ -13,10 +14,16 @@ document.querySelector('#search-button').onclick = (e) => {
     searchBox.focus();
     e.preventDefault();
 };
+document.querySelector('#login-button').onclick = (e) => {
+    loginMenu.classList.toggle('active');
+    e.preventDefault();
+};
 
 // remove sidebar
 const hamburger = document.querySelector('#hamburger-menu');
 const searchButton = document.querySelector('#search-button');
+const loginButton = document.querySelector('#login-button');
+const loginBox = document.querySelector('#login-box');
 
 document.addEventListener('click', function(e) {
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
@@ -26,6 +33,11 @@ document.addEventListener('click', function(e) {
 document.addEventListener('click', function(e) {
     if(!searchMenu.contains(e.target) && !searchButton.contains(e.target)){
         searchMenu.classList.remove('active');
+    }
+});
+document.addEventListener('click', function(e) {
+    if(!loginBox.contains(e.target) && !loginButton.contains(e.target)){
+        loginMenu.classList.remove('active');
     }
 });
 
