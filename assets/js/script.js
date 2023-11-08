30ba20f7bc31ae6d58edd687ea718604b05375ad
AufanT <aufantaufiq08905@gmail.com>
2023-11-08 17:05:53 +0700

    menambahkan fitur search menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,32 @@
 // toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
+const searchMenu = document.querySelector('.search-menu')
+const navbar = document.querySelector('.navbar')
+const searchBox = document.querySelector('#search-box')
 
 document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
-    e.preventDefault()
+    e.preventDefault();
+};
+document.querySelector('#search-button').onclick = (e) => {
+    searchMenu.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
 };
 
 // remove sidebar
 const hamburger = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
 
 document.addEventListener('click', function(e) {
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
 });
+document.addEventListener('click', function(e) {
+    if(!searchMenu.contains(e.target) && !searchButton.contains(e.target)){
+        searchMenu.classList.remove('active');
+    }
+});
+
+
